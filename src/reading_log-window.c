4465f6cf4ee9a7270a1b001f9c56d98df930ec67
andersonjwan <andersonjwan@outlook.com>
2020-04-22 21:39:29 -0700

    changed reference from reading_log-window.ui to main.ui

--- src/reading_log-window.c
+++ src/reading_log-window.c
@@ -23,9 +23,6 @@ struct _ReadingLogWindow
 {
   GtkApplicationWindow  parent_instance;
 
-  /* Template widgets */
-  GtkHeaderBar        *header_bar;
-  GtkLabel            *label;
 };
 
 G_DEFINE_TYPE (ReadingLogWindow, reading_log_window, GTK_TYPE_APPLICATION_WINDOW)
@@ -35,9 +32,7 @@ reading_log_window_class_init (ReadingLogWindowClass *klass)
 {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/andersonjwan/ReadingLog/reading_log-window.ui");
-  gtk_widget_class_bind_template_child (widget_class, ReadingLogWindow, header_bar);
-  gtk_widget_class_bind_template_child (widget_class, ReadingLogWindow, label);
+  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/andersonjwan/ReadingLog/ui/main.ui");
 }
 
 static void
